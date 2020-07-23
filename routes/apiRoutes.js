6ca6a718df1f50e6b5fb1acb9ebb7699287a3e6d
ForaceGump <themattavant@gmail.com>
2020-07-22 20:41:25 -0700

    Finished setting up server.js and fixed problem with models not properly working by adding a seperate workout.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const db = require("../models");
+const mongojs = require("mongojs");
+
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({day : {$gte: new Date().setDate(new Date().getDate()-7)}})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+
+  router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $push: { 
+          exercises: req.body
+        } 
+      },
+      (error, newEntry) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        } else {
+          console.log(newEntry);
+          res.send(newEntry);
+        }
+      }
+    );
+  });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
